a8c0d94152bc7b545f7c977541b33a8ddbdb6e32
Laura Levenhagen <Laura@Lauras-MBP.attlocal.net>
2018-08-24 21:24:21 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,19 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[1];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[0];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
-	} else { alert("Sorry, try again.")}
+	} else { alert("Sorry, try again.")}	
 }
+var flipCard = function(cardId) {
+if (cardsInPlay.length === 2) {
+checkForMatch();
+cardsInPlay.push(cards[cardId]);
+}
+console.log("User flipped " + cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+
+

c372f937332692e8b4a5f988ac6008a4b78aaa5b
Laura Levenhagen <Laura@Lauras-MBP.attlocal.net>
2018-08-27 14:44:09 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,7 +26,9 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else { alert("Sorry, try again.")}	
 };
-var flipCard = function(cardId) {
+var flipCard = function() {
+cardId = this.getAttribute('data-id');
+this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 checkForMatch();
 cardsInPlay.push(cards[cardId].rank);
@@ -35,8 +37,16 @@ console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+createBoard();
 
 
 
